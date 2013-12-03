b32880fa3a7817586729280d42709274dcc82cc7
Jorge Diaz <jorgedf@sixclicks.net>
2013-12-03 13:30:11 +0100

    Landing pages arrangements.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-  	render :file => 'public/landing-app'
+  	render :file => 'public/landing-app', :layout => false
   end
 
 end
